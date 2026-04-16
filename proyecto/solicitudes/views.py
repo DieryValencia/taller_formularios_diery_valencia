5d1e1e202ec8c9d0e68832294d8d97febfc18888
DieryValencia <diery.valencia@campusucc.edu.co>
2026-04-16 11:32:45 -0500

    app solicitudes

--- proyecto/solicitudes/views.py
+++ proyecto/solicitudes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SolicitudForm
 
-# Create your views here.
+def solicitud_formulario(request):
+    if request.method == 'POST':
+        form = SolicitudForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('solicitud_confirmacion')
+    else:
+        form = SolicitudForm()
+    return render(request, 'solicitudes/formulario.html', {'form': form})
+
+def solicitud_confirmacion(request):
+    return render(request, 'solicitudes/confirmacion.html')
